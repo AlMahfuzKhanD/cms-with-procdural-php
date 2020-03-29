ed9e3661cb40015a762809078ee8a5f5bdaa2591
mahfuz380 <almahfuz380@gmail.com>
2020-03-29 09:02:29 +0600

    connection created

---
+++ includes/db.php
@@ -0,0 +1,17 @@
+<?php
+$db['dbHost'] = "localhost";    //for better security the data of database is sent as a constant data using array
+$db['dbUser'] = "root";
+$db['dbPass'] = "";
+$db['dbName'] = "cms";
+
+foreach ($db as $key => $value) {
+	 define(strtoupper($key), $value);
+}
+
+
+
+$connection = mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
+if($connection){
+	echo "we are connected";
+}
+?>
