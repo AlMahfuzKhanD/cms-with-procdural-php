93d6fb7ceccb54a185cb47731abd04a6d4d373be
mahfuz380 <almahfuz380@gmail.com>
2020-04-03 10:19:15 +0600

    login front end design added in sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -20,6 +20,24 @@
 					<!-- /.input-group -->
 				</div>
 
+				<!-- Blog Search Well -->
+				<div class="well">
+					<h4>Login Here</h4>
+					<form action="includes/login.php" method="post">
+					<div class="form-group">
+						<input placeholder="Enter User Name" name="userName" type="text" class="form-control"></div>
+					<div class="input-group">
+						<input placeholder="Enter User Password" name="userPassword" type="password" class="form-control">
+						
+						<span class="input-group-btn">
+							<button name="createLogin" class="btn btn-primary" type="submit">
+								<span>Submit</span>
+						</button>
+					</div>
+				</form> <!--search form -->
+					<!-- /.input-group -->
+				</div>
+
 				<!-- Blog Categories Well -->
 				<div class="well">
 
