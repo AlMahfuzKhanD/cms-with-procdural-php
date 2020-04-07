52eae0fbf318d7665555e921f785a9bf62090321
mahfuz380 <almahfuz380@gmail.com>
2020-04-07 14:50:48 +0600

    some reefactoring done for running jquery

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -1,9 +1,17 @@
  </div>
     <!-- /#wrapper -->
 
+    <script>
+    	ClassicEditor
+        .create( document.querySelector( '#body' ) )
+        .catch( error => {
+            console.error( error );
+} ); // end classicEditor
+    </script>
+
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-    <script src="js/scripts.js"></script>
+    <script src="js/script.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
