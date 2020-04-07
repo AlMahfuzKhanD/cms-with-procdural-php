504b5fb3a95cc266de0b58e1fdd420218c1a1642
mahfuz380 <almahfuz380@gmail.com>
2020-04-07 14:49:30 +0600

    script file renamed

---
+++ admin/js/script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function () {
+
+    // script for selecting all post at a time
+    $('#selectAllBoxes').click(function(event) {
+        if(this.checked){
+            $('.checkBoxes').each(function () {
+                this.checked = true;
+            });
+        }else{
+            $('.checkBoxes').each(function () {
+                this.checked = false;
+            });
+        }
+    });
+
+    alert('hello');
+
+
+});
+	    
