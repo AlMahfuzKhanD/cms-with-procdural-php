83e21c11178db852dfa9df26e2b2a1de76131c11
mahfuz380 <almahfuz380@gmail.com>
2020-04-10 19:23:16 +0600

    some modification done on loader

--- admin/js/script.js
+++ admin/js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     
 var div_box = "<div id='load-screen'><div id='loading'></div></div>";
 $("body").prepend(div_box);
-$('#load-screen').delay(17000).fadeOut(16000, function(){
+$('#load-screen').delay(7000).fadeOut(6000, function(){
    $(this).remove();
 });
      
