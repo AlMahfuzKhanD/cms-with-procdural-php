5e38c6074e52f4111ec04bb17ffa100558246529
mahfuz380 <almahfuz380@gmail.com>
2020-04-10 21:15:42 +0600

    loader time reduced

--- admin/js/script.js
+++ admin/js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     
 var div_box = "<div id='load-screen'><div id='loading'></div></div>";
 $("body").prepend(div_box);
-$('#load-screen').delay(7000).fadeOut(6000, function(){
+$('#load-screen').delay(700).fadeOut(600, function(){
    $(this).remove();
 });
      
