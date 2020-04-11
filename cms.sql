78ad701599f62a90eaf686715f6b601b7cd0e9e0
mahfuz380 <almahfuz380@gmail.com>
2020-04-11 13:51:09 +0600

    sql updated

--- cms.sql
+++ cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2020 at 08:36 AM
+-- Generation Time: Apr 11, 2020 at 09:00 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -88,17 +88,35 @@ CREATE TABLE `posts` (
   `postContent` text NOT NULL,
   `postTags` varchar(255) NOT NULL,
   `postCommentCount` int(11) NOT NULL,
-  `postStatus` varchar(255) NOT NULL DEFAULT 'draft'
+  `postStatus` varchar(255) NOT NULL DEFAULT 'draft',
+  `postViewsCount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`postId`, `postCatagoryId`, `postTitle`, `postAuthor`, `postDate`, `postImage`, `postContent`, `postTags`, `postCommentCount`, `postStatus`) VALUES
-(4, 1, 'title', 'author', '2020-04-05', 'Jellyfish.jpg', 'ddddddddddddddddddddddddd', 'tags', 6, 'published'),
-(5, 1, 'new post', 'author', '2020-04-05', 'Hydrangeas.jpg', 'new post check', 'java, php', 0, 'published'),
-(6, 1, 'title', 'me', '2020-04-07', 'image_1.jpg', '<p>ljlaj asjsdlf lk al l ala lallfj a vvvvvvvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvvvvvv dddddddddddddddddddddd ssssssssssssssssss aaaaaaaaaaaaa</p>', 'java', 0, 'published');
+INSERT INTO `posts` (`postId`, `postCatagoryId`, `postTitle`, `postAuthor`, `postDate`, `postImage`, `postContent`, `postTags`, `postCommentCount`, `postStatus`, `postViewsCount`) VALUES
+(4, 1, 'title', 'author', '2020-04-05', 'Jellyfish.jpg', 'ddddddddddddddddddddddddd', 'tags', 6, 'published', 0),
+(5, 1, 'new post', 'author', '2020-04-05', 'Hydrangeas.jpg', 'new post check', 'java, php', 0, 'published', 0),
+(6, 1, 'title', 'me', '2020-04-07', 'image_1.jpg', '<p>ljlaj asjsdlf lk al l ala lallfj a vvvvvvvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvvvvvv dddddddddddddddddddddd ssssssssssssssssss aaaaaaaaaaaaa</p>', 'java', 0, 'published', 0),
+(8, 1, 'poststatus checking', 'author', '2020-04-07', 'image_1.jpg', '<p>java is awesome but php is best java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;</p>', 'java', 0, 'published', 0),
+(9, 1, 'view post check', 'me', '2020-04-07', 'Penguins.jpg', '<p>dddd view post and view all post checking</p>', 'tags', 0, 'published', 0),
+(10, 1, 'view post check', 'me', '2020-04-07', 'Penguins.jpg', '<p>checking checking&nbsp;checking&nbsp;checking&nbsp;checking&nbsp;checking&nbsp;checking&nbsp;checking&nbsp;checking&nbsp;</p>', 'tags', 0, 'published', 0),
+(11, 1, 'view all post checking', 'me', '2020-04-07', 'Hydrangeas.jpg', '<p>view all post checking view all post checking&nbsp;view all post checking&nbsp;view all post checking&nbsp;view all post checking&nbsp;</p>', 'tags', 0, 'published', 0),
+(12, 1, 'title', 'me', '2020-04-10', 'image_1.jpg', '<p>ljlaj asjsdlf lk al l ala lallfj a vvvvvvvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvvvvvv dddddddddddddddddddddd ssssssssssssssssss aaaaaaaaaaaaa</p>', 'java', 0, 'published', 0),
+(13, 1, 'poststatus checking', 'author', '2020-04-10', 'image_1.jpg', '<p>java is awesome but php is best java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;</p>', 'java', 0, 'published', 0),
+(14, 1, 'title', 'author', '2020-04-10', 'Jellyfish.jpg', 'ddddddddddddddddddddddddd', 'tags', 0, 'published', 0),
+(15, 1, 'title', 'me', '2020-04-10', 'image_1.jpg', '<p>ljlaj asjsdlf lk al l ala lallfj a vvvvvvvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvvvvvv dddddddddddddddddddddd ssssssssssssssssss aaaaaaaaaaaaa</p>', 'java', 0, 'published', 0),
+(16, 1, 'new post', 'author', '2020-04-10', 'Hydrangeas.jpg', 'new post check', 'java, php', 0, 'published', 0),
+(17, 1, 'view post check', 'me', '2020-04-10', 'Penguins.jpg', '<p>dddd view post and view all post checking</p>', 'tags', 0, 'published', 0),
+(18, 1, 'poststatus checking', 'author', '2020-04-10', 'image_1.jpg', '<p>java is awesome but php is best java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;</p>', 'java', 0, 'published', 0),
+(19, 1, 'poststatus checking', 'author', '2020-04-11', 'image_1.jpg', '<p>java is awesome but php is best java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;</p>', 'java', 0, 'published', 0),
+(20, 1, 'view post check', 'me', '2020-04-11', 'Penguins.jpg', '<p>dddd view post and view all post checking</p>', 'tags', 0, 'published', 0),
+(21, 1, 'poststatus checking', 'author', '2020-04-11', 'image_1.jpg', '<p>java is awesome but php is best java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;</p>', 'java', 0, 'published', 0),
+(22, 1, 'view post check', 'me', '2020-04-11', 'Penguins.jpg', '<p>dddd view post and view all post checking</p>', 'tags', 0, 'published', 0),
+(23, 1, 'poststatus checking', 'author', '2020-04-11', 'image_1.jpg', '<p>java is awesome but php is best java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;java is awesome but php is best&nbsp;</p>', 'java', 0, 'published', 0),
+(24, 1, 'view post check', 'me', '2020-04-11', 'Penguins.jpg', '<p>dddd view post and view all post checking</p>', 'tags', 0, 'published', 0);
 
 -- --------------------------------------------------------
 
@@ -115,7 +133,7 @@ CREATE TABLE `users` (
   `userEmail` varchar(255) NOT NULL,
   `userImage` text NOT NULL,
   `userRole` varchar(255) NOT NULL,
-  `randSalt` varchar(255) NOT NULL,
+  `randSalt` varchar(255) NOT NULL DEFAULT '$2y$10$iusesomecrazystrings22',
   `userStatus` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -124,10 +142,21 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userId`, `userName`, `userPassword`, `userFirstName`, `userLastName`, `userEmail`, `userImage`, `userRole`, `randSalt`, `userStatus`) VALUES
-(3, 'padeka', 'sadeka', 'mahfuzedit', 'khan', 'sadekau@kaua.com', '', 'Subscriber', '', 'Denied'),
-(4, 'mahfuz1', '11111', 'mahfuz12', 'khan', 'sadekau@kaua.com', '', 'Admin', '', 'Denied'),
-(5, 'mahfuz380', '123', 'Al', 'khan', 'a@a.com', '', 'Admin', '', 'Denied'),
-(6, 'edwindiaz', '12345', 'adwin', 'diaz', 'ediwn@edwin.com', '', 'Admin', '', 'Denied');
+(6, 'edwindiaz', '$1$fcGvkj0J$3mtNmkiqsOwwn469z7lhl1', 'adwin', 'diaz', 'ediwn@edwin.com', '', 'Admin', '', 'Denied'),
+(7, 'newUser', '$1$JMTzM3SH$QvUj.rNA.d9TFeUzz2noM0', 'new', 'user', 'ediwn@edwin.com', '', 'Admin', '$2y$10$iusesomecrazystrings22', 'Denied'),
+(12, 'demo', '$1$X3Rdfzdk$YKY./vsX.hJFMwF114nuM/', 'demo', 'check', 'demo@demo.com', '', 'Admin', '$2y$10$iusesomecrazystrings22', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usersonline`
+--
+
+CREATE TABLE `usersonline` (
+  `id` int(11) NOT NULL,
+  `session` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -158,6 +187,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`userId`);
 
 --
+-- Indexes for table `usersonline`
+--
+ALTER TABLE `usersonline`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -177,13 +212,19 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `postId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `postId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `userId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `usersonline`
+--
+ALTER TABLE `usersonline`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
