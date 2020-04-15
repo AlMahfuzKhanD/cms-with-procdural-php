1f49695c9f1dc7e981bceedad4aeb8d0a218a2ae
mahfuz380 <almahfuz380@gmail.com>
2020-04-15 20:57:42 +0600

    login for disappear if user logged in

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,86 +3,90 @@
 
 
                 
+<div class="well">
+		<h4>Blog Search</h4>
+		<form action="search.php" method="post">
+		<div class="input-group">
+			<input name="search" type="text" class="form-control">
+			<span class="input-group-btn">
+				<button name="submit" class="btn btn-default" type="submit">
+					<span class="glyphicon glyphicon-search"></span>
+			</button>
+			</span>
+		</div>
+	</form> <!--search form -->
+		<!-- /.input-group -->
+</div>
 
-				<!-- Blog Search Well -->
-				<div class="well">
-					<h4>Blog Search</h4>
-					<form action="search.php" method="post">
-					<div class="input-group">
-						<input name="search" type="text" class="form-control">
-						<span class="input-group-btn">
-							<button name="submit" class="btn btn-default" type="submit">
-								<span class="glyphicon glyphicon-search"></span>
-						</button>
-						</span>
-					</div>
-				</form> <!--search form -->
-					<!-- /.input-group -->
-				</div>
+<!-- Blog Search Well -->
+<?php
 
-				<!-- Blog Search Well -->
-				<div class="well">
-					<h4>Login Here</h4>
-					<form action="includes/login.php" method="post">
-					<div class="form-group">
-						<input placeholder="Enter User Name" name="userName" type="text" class="form-control"></div>
-					<div class="input-group">
-						<input placeholder="Enter User Password" name="userPassword" type="password" class="form-control">
-						
-						<span class="input-group-btn">
-							<button name="createLogin" class="btn btn-primary" type="submit">
-								<span>Submit</span>
-						</button>
-					</div>
-				</form> <!--search form -->
-					<!-- /.input-group -->
-				</div>
+if(isset($_SESSION['userName'])){ ?>
+	 <h1>Logged in as <?php echo $_SESSION['userName']; ?></h1>
+	 <a href='includes/logout.php' class='btn btn-primary'>Logout</a>
+<?php }else{ ?>
 
-				<!-- Blog Categories Well -->
-				<div class="well">
+<div class="well">
+	<h4>Login Here</h4>
+	<form action="includes/login.php" method="post">
+		<div class="form-group">
+			<input placeholder="Enter User Name" name="userName" type="text" class="form-control"></div>
 
-				<?php 
+		<div class="input-group">
+			<input placeholder="Enter User Password" name="userPassword" type="password" class="form-control">
+			
+			<span class="input-group-btn">
+				<button name="createLogin" class="btn btn-primary" type="submit">
+					<span>Submit</span>
+			</button>
+		</div>
+	</form> <!--log in form -->
+	<!-- /.input-group -->
+</div> <?php } ?>
 
-				$query = "SELECT * FROM catagories";
-				$selectCatagoriesSidebar = mysqli_query($connection,$query);
+	<!-- Blog Categories Well -->
+<div class="well">
 
+<?php 
 
-				?>
-
-
-
-					<h4>Blog Categories</h4>
-					<div class="row">
-						<div class="col-lg-12">
-							<ul class="list-unstyled">
-
-								<?php 
-
-								while($row = mysqli_fetch_assoc($selectCatagoriesSidebar)){
-								$catTitle = $row['catTitle'];
-								$catId = $row['catId'];
-								echo "<li><a href='catagory.php?catagory=$catId'>{$catTitle}</a></>";
-								}
-
-								?>
-
-							</ul>
-						</div>
+$query = "SELECT * FROM catagories";
+$selectCatagoriesSidebar = mysqli_query($connection,$query);
 
 
-						
+?>
 
-						
-					</div>
-					<!-- /.row -->
-				</div>
+<h4>Blog Categories</h4>
+<div class="row">
+<div class="col-lg-12">
+	<ul class="list-unstyled">
+
+		<?php 
+
+		while($row = mysqli_fetch_assoc($selectCatagoriesSidebar)){
+		$catTitle = $row['catTitle'];
+		$catId = $row['catId'];
+		echo "<li><a href='catagory.php?catagory=$catId'>{$catTitle}</a></>";
+		}
+
+		?>
+
+	</ul>
+</div>
+
+
+			
+
+			
+		</div>
+		<!-- /.row -->
+	</div>
 
 
 
 
-				<!-- Side Widget Well -->
-				<?php include "widget.php"; ?>
+	<!-- Side Widget Well -->
+	<?php include "widget.php"; ?>
 
-			</div>
+</div>
 
 		
